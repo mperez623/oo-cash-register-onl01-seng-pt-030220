4fefdad76c9c69ecd551eaab6700b7ba857201ea
Melissa Perez <melissaperezg.96@gmail.com>
2020-04-05 17:52:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -34,6 +34,7 @@ class CashRegister
   def void_last_transaction
     @total -= self.last_transaction[1] * self.last_transaction[2]
     self.last_transaction do 
+      self.items.delete 
     
   end
   
