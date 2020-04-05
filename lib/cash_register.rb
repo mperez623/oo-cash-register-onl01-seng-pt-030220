f8fac81f65b4196a0348f639f322671fab47404b
Melissa Perez <melissaperezg.96@gmail.com>
2020-04-05 18:05:14 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ class CashRegister
   
   def add_item(title, price, quantity = 1)
     @total += price*quantity
-    quantity.times {self.items << title}
+    quantity.times { self.items << title }
     last_transaction = [title, price, quantity]
      binding.pry
   end
