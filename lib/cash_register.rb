208c0302ed43d8ee78f57b3b4c579d7fc3b02868
Melissa Perez <melissaperezg.96@gmail.com>
2020-04-05 18:04:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,6 +14,7 @@ class CashRegister
     @total += price*quantity
     quantity.times {self.items << title}
     last_transaction = [title, price, quantity]
+     binding.pry
   end
   
   def apply_discount 
